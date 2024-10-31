a10bb29ecce5c1dc043f0c37d9ef6b5f71cfd52e
Wiliam Roza <devfrontwill@gmail.com>
2024-10-31 16:35:58 -0300

    Feat: api key safe

--- src/services/CreateNutritionService.ts
+++ src/services/CreateNutritionService.ts
@@ -1,9 +1,22 @@
 import { DataProps } from '../controllers/CreateNutritionController';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
 class CreateNutritionService{
     async execute({ name, weight, height, age, gender, objective, level }: DataProps){
 
-        console.log("TESTE")
+        try {
+            const genAI = new GoogleGenerativeAI(process.env.API_KEY!)
+            const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+
+            const response = await model.generateContent(`Em que ano o JavaScript foi criado`)
+            console.log(JSON.stringify(response, null, 2));
+
+            return { ok: true }
+            
+        } catch (err) {
+            console.log("Erro JSON ", err)
+            throw new Error("Failed to create. ")
+        }
 
         return { message: "FUNCIONOU O SERVICE" }
     }
